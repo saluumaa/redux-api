0bed038be5f2150cd310a12284401d4b58bf0cd3
saluumaa <salumahassan02@gmail.com>
2023-04-13 18:10:18 +0300

    Add redux store

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import UserFetched from './store/users/userFetched';
+function App() {
+  return (
+    <div className="App">
+      <UserFetched />
+    </div>
+  );
+}
+
+export default App;
